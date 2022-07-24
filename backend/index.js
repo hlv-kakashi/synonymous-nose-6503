69136723717582908b02ca84e6151a839c412177
hlv-kakashi <100180990+hlv-kakashi@users.noreply.github.com>
2022-07-24 16:58:57 +0530

    .env file update

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,13 @@ const cors = require("cors");
 const productRoute = require("./routes/productRoutes");
 const mongoose = require("mongoose");
 const AuthRoute= require("./routes/authRoutes")
+
 //<---------------------------------------------------------------->
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+require("dotenv").config();
 
 //<---------------------------------------------------------------->
 //Add routes here
@@ -16,8 +18,8 @@ app.use("/", productRoute);
 app.use("/auth", AuthRoute)
 
 //<---------------------------------------------------------------->
-const CONNECTION_URL = 'mongodb+srv://hlv-kakashi:Anupam123@cluster0.tjkwwu3.mongodb.net/?retryWrites=true&w=majority';
+
 const PORT = process.env.PORT|| 8080;
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
